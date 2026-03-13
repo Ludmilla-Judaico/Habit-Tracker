6194d05608ec4750977782eed13a584199d9829a
Ludmilla <ludmillajudaico@gmail.com>
2026-03-13 13:04:23 -0300

    Feat: Add new Habits

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="flex flex-col justify-center items-center p-7"
+        className=""
         id="wallpaper"
       >
         {children}
